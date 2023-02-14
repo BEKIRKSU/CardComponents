1f1f5df4ca8309a1b224b240a8c97058ba7db4cc
BEKIRKSU <113244972+BEKIRKSU@users.noreply.github.com>
2023-02-14 14:42:55 +0000

    Update contacts

--- contacts.js
+++ contacts.js
@@ -3,27 +3,27 @@
 const contacts = [
     {
       id: 1,
-      name: "Beyonce",
+      name: "Rihanna",
       imgURL:
-        "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
-      phone: "+123 456 789",
-      email: "b@beyonce.com"
+        "https://s2.r29static.com/bin/entry/9ee/0,201,2000,2000/x,80/1577381/image.jpg",
+      phone: "+1982 9873 273",
+      email: "r@brihanna.com"
     },
     {
       id: 2,
-      name: "Jack Bauer",
+      name: "Ronaldinho",
       imgURL:
-        "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
-      phone: "+987 654 321",
-      email: "jack@nowhere.com"
+        "https://futboltriangle.files.wordpress.com/2013/08/ronaldinhobarcelonavstuttgartuefachampionsj2eetbnohsyl.jpg",
+      phone: "+4522 1265 235",
+      email: "ro@naldinho.com"
     },
     {
       id: 3,
-      name: "Chuck Norris",
+      name: "Audrey Hepburn",
       imgURL:
-        "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
-      phone: "+918 372 574",
-      email: "gmail@chucknorris.com"
+        "https://www.therealpearlco.com/wp-content/uploads/2020/11/405E7423-522C-4886-9A04-EBB6757758E2.jpeg",
+      phone: "+5782 8734 641",
+      email: "audrey@hepburn.com"
     }
   ];
   
